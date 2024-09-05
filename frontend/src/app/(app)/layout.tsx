56106d93420276341c805c22fd4ffdcf2d376216
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-09-05 17:38:20 +0800

    remove redirect to clerk hosted site

--- frontend/src/app/(app)/layout.tsx
+++ frontend/src/app/(app)/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
-import "./globals.css";
+import "../globals.css";
+import { Navbar } from "@/components/navbar";
+import { ModeToggle } from "@/components/toggle-theme-button";
 import { cn } from "@/lib/utils";
 import { fontSans } from "@/lib/fonts";
 import { ClerkProvider } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "IS442 Project",
@@ -16,14 +19,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
       <html lang="en">
         <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <div className="flex flex-row justify-between p-4 items-center">
+              <Navbar className="mx-6" />
+              <div className="flex flex-row gap-4 items-center">
+                <ModeToggle />
+                <SignedOut>
+                  <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                  <UserButton />
+                </SignedIn>
+              </div>
+            </div>
             {children}
           </ThemeProvider>
         </body>
       </html>
-    </ClerkProvider>
   );
 }
