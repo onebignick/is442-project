76fa27e9e249d08aa2bc0af7c1421c3110647e91
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-09-05 16:35:41 +0800

    add navbar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,9 @@
-import { ModeToggle } from "@/components/toggle-theme-button";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <Button>Click me</Button>
-      <ModeToggle />
     </div>
   );
 }
