31b5b9756f9cbc7e0d656bddb77e934ad1955e5e
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-09-14 12:18:39 +0800

    Update DB mdoel to reflect price

--- frontend/src/schema.ts
+++ frontend/src/schema.ts
@@ -1,4 +1,4 @@
-import { pgTableCreator, varchar } from "drizzle-orm/pg-core";
+import { pgTableCreator, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const createTable = pgTableCreator((name) => `is442_${name}`);
 
@@ -11,3 +11,34 @@ export const user = createTable("user", {
 	lastName: varchar("last_name", {length: 32 }),
 	username: varchar("username", { length: 32 }),
 });
+
+export const customer = createTable("customer", {
+	id: uuid("id").defaultRandom().primaryKey(),
+})
+
+export const saleType = createTable("sale_type", {
+	id: uuid("id").defaultRandom().primaryKey(),
+	saleType: varchar("sale_type", {length: 32}),
+})
+
+export const product = createTable("product", {
+	id: varchar("id", {length: 32}).primaryKey(),
+	productName: varchar("product_name", { length: 100 }),
+})
+
+export const price = createTable("price", {
+	id: varchar("id", {length:32}).primaryKey(),
+	price: varchar("price", {length:32}),
+	productId: varchar("product_id", {length:32}).references(()=>product.id)
+})
+
+export const order = createTable("order", {
+	id: uuid("id").defaultRandom().primaryKey(),
+})
+
+export const orderLineItem = createTable("order_line_item", {
+	id: uuid("id").defaultRandom().primaryKey(),
+	orderId: uuid("order_id").references(() => order.id),
+	customerId: uuid("customer_id").references(() => customer.id),
+	priceId: varchar("price_id", {length:32}).references(()=>price.id)
+})
