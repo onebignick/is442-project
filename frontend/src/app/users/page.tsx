cbb32a88a9ee79e021600f0b450115dcbd96eb88
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-11-06 17:23:36 +0800

    fetch all users

--- frontend/src/app/users/page.tsx
+++ frontend/src/app/users/page.tsx
@@ -1,37 +1,22 @@
 import { UserDataTable } from "@/components/datatables/users/UserDataTable";
 import { UserDataTableColumns } from "@/components/datatables/users/UserDataTableColumns";
 
-export default function UserPage() {
-    const users = [
-        {
-            username: "nicholas",
-            password: "ong",
-            roles: ["admin"]
-        },
-        {
-            username: "sherry",
-            password: "koo",
-            roles: ["marketing"]
-        },
-        {
-            username: "danelle",
-            password: "goh",
-            roles: ["sales"]
-        },
-        {
-            username: "dexter",
-            password: "chua",
-            roles: ["sales", "marketing"]
-        },
-        {
-            username: "hao ran",
-            password: "tong",
-            roles: ["admin", "marketing"]
-        },
-    ]
-    return (
-        <div>
-            <UserDataTable columns={UserDataTableColumns} data={users}/>
-        </div>
-    )
+export default async function UserPage() {
+    const req = await fetch("http://localhost:8080/api/users", {
+        method: "GET"
+    })
+    
+    if (req.ok) {
+        const users = await req.json();
+        
+        return (
+            <div>
+                <UserDataTable columns={UserDataTableColumns} data={users}/>
+            </div>
+        )
+    } else {
+        return (
+            <p>An error occured</p>
+        )
+    }
 }
