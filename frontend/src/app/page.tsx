581e5d8af364f7609219096b74661bd7df0149bc
sherrykjy <helloitsree@gmail.com>
2024-11-06 19:02:39 +0800

    breadcrumb component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,26 +1,29 @@
 "use client"
 import React from 'react';
 import ActionButton from "@/components/action-button";
+// import UserDataTable from "@/components/datatables/users/UserDataTable";
+
+import { BreadcrumbComponent } from '@/components/breadcrumb-component';
 
 export default function Page() {
-    const handleCreate = () => {
-        console.log('create button clicked');
-    };
-
-    const handleDelete = () => {
-        console.log('delete button clicked');
-    };
+    const breadcrumbItems = [
+        { label: "Home", href: "/" },
+    ]
 
     return (
         <div style={{ padding: '20px' }}>
             <h1>Test Page</h1>
 
+            <BreadcrumbComponent items={breadcrumbItems} />
+
+            {/* <UserDataTable></UserDataTable> */}
+
             <div style={{ marginBottom: '10px' }}>
-                <ActionButton label="Create" variant="create" onClick={handleCreate} />
+                <ActionButton label="Create" variant="create" />
             </div>
 
             <div>
-                <ActionButton label="Delete" variant="delete" onClick={handleDelete} />
+                <ActionButton label="Delete" variant="delete" />
             </div>
 
             <br />
--- frontend/src/app/users/page.tsx
+++ frontend/src/app/users/page.tsx
@@ -3,36 +3,48 @@ import { UserDataTableColumns } from "@/components/datatables/users/UserDataTabl
 import { FormDialogButton } from "@/components/form-dialog-button";
 import { CreateUserForm } from "@/components/forms/create-user-form";
 import { UserDTO } from "@/types/UserDTO";
+import { BreadcrumbComponent } from '@/components/breadcrumb-component';
 
 export default async function UserPage() {
-    const req = await fetch("http://localhost:8080/api/users", {
-        method: "GET"
-    })
+    try {
+        const breadcrumbItems = [
+            { label: "Home", href: "/" },
+            { label: "Users", href: "/users" },
+        ]
     
-    if (req.ok) {
-        const usersDTO = await req.json();
-        for (let i=0;i<usersDTO.length;i++) {
-            usersDTO[i] = new UserDTO(
-                usersDTO[i].username,
-                usersDTO[i].password,
-                usersDTO[i].roles
-            ).toUser();
-        }
+        const req = await fetch("http://localhost:8080/api/users", {
+            method: "GET"
+        })
         
+        if (req.ok) {
+            const usersDTO = await req.json();
+            for (let i=0;i<usersDTO.length;i++) {
+                usersDTO[i] = new UserDTO(
+                    usersDTO[i].username,
+                    usersDTO[i].password,
+                    usersDTO[i].roles
+                ).toUser();
+            }
+            
+            return (
+                <div>
+                    <BreadcrumbComponent items={breadcrumbItems} />
+
+                    <FormDialogButton
+                        title="Create a user"
+                        label="Create a user"
+                        description="Fill in the form below to create a new user"
+                        form={<CreateUserForm/>}
+                    />
+                    <UserDataTable columns={UserDataTableColumns} data={usersDTO}/>
+                </div>
+            )
+        }
+    }
+
+    catch {
         return (
-            <div>
-                <FormDialogButton
-                    title="Create a user"
-                    label="Create a user"
-                    description="Fill in the form below to create a new user"
-                    form={<CreateUserForm/>}
-                />
-                <UserDataTable columns={UserDataTableColumns} data={usersDTO}/>
-            </div>
-        )
-    } else {
-        return (
-            <p>An error occured</p>
+            <p> An error occured :( </p>
         )
     }
 }
