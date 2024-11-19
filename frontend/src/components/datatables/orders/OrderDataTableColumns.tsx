99c4992b68f4190b85ed3b7d3d7f50f538047c5e
dg0h <danellegoh.2021@scis.smu.edu.sg>
2024-11-19 23:21:32 +0800

    add checkbox for OrderDataTable

--- frontend/src/components/datatables/orders/OrderDataTableColumns.tsx
+++ frontend/src/components/datatables/orders/OrderDataTableColumns.tsx
@@ -3,9 +3,31 @@ import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Order } from "@/types/Order";
-
+import { Checkbox } from "@/components/ui/checkbox";
 
 export const OrderDataTableColumns: ColumnDef<Order>[] = [
+    {
+        id: "select",
+        header: ({ table }) => (
+        <Checkbox
+            checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+            }
+            onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+            aria-label="Select all"
+        />
+        ),
+        cell: ({ row }) => (
+        <Checkbox
+            checked={row.getIsSelected()}
+            onCheckedChange={(value) => row.toggleSelected(!!value)}
+            aria-label="Select row"
+        />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+    },
     {
         accessorKey: "order_id",
         header: ({ column }) => {
