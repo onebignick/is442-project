fdfacaa66a4633fda39dcdbf23e0c6063001eb04
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-11-19 21:48:15 +0800

    rbac

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,11 +1,30 @@
+// @ts-ignore
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/api/webhook/clerk"])
+const isAdminRoute = createRouteMatcher(["/apps/admin(.*)"])
+const isMarketingRoute = createRouteMatcher(["/apps/marketing(.*)"])
+const isSalesRoute = createRouteMatcher(["/apps/sales(.*)"])
 
 export default clerkMiddleware((auth, request) => {
+
   if (!isPublicRoute(request)) {
     auth().protect()
   }
+  if (isAdminRoute(request) && auth().sessionClaims?.public_metadata?.role != "admin") {
+    const url = new URL("/", request.url);
+    return NextResponse.redirect(url);
+  }
+  if (isMarketingRoute(request) && auth().sessionClaims?.public_metadata?.role != "marketing") {
+    const url = new URL("/", request.url);
+    return NextResponse.redirect(url);
+  }
+  if (isSalesRoute(request) && auth().sessionClaims?.public_metadata?.role != "sales") {
+    const url = new URL("/", request.url);
+    return NextResponse.redirect(url);
+  }
+
 });
 
 export const config = {
